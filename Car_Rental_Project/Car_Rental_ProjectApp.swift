1618dcfbaca8f4b9cd8cf1604b6f9fbcd5c60c51
p.bielawski <piotr.bielawski@onet.pl>
2022-03-29 14:12:17 +0200

    Initial Commit

---
+++ Car_Rental_Project/Car_Rental_ProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  Car_Rental_ProjectApp.swift
+//  Car_Rental_Project
+//
+//  Created by Piotrek Bielawski on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Car_Rental_ProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
